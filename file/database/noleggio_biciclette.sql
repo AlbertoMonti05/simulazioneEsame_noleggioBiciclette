37d429cde695f8d62307a059a30d1c6fad6e3b94
AlbertoMonti05 <20albymonti05@gmail.com>
2024-04-26 10:04:04 +0200

    Aggiornamento del database

--- file/database/noleggio_biciclette.sql
+++ file/database/noleggio_biciclette.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 25, 2024 alle 14:40
+-- Creato il: Apr 26, 2024 alle 10:03
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -24,6 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struttura della tabella `biciclette`
+--
+
+CREATE TABLE `biciclette` (
+  `bicicletta_id` int(11) NOT NULL,
+  `latitudine` double NOT NULL,
+  `longitudine` double NOT NULL,
+  `km_percorsi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `carte_credito`
+--
+
+CREATE TABLE `carte_credito` (
+  `carta_credito_id` int(11) NOT NULL,
+  `nome_titolare` varchar(32) NOT NULL,
+  `cognome_titolare` varchar(32) NOT NULL,
+  `numero` char(16) NOT NULL,
+  `scadenza` date NOT NULL,
+  `cvv` char(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dump dei dati per la tabella `carte_credito`
+--
+
+INSERT INTO `carte_credito` (`carta_credito_id`, `nome_titolare`, `cognome_titolare`, `numero`, `scadenza`, `cvv`) VALUES
+(1, 'mario', 'rossi', '1234567891234567', '2028-04-17', '000');
+
+-- --------------------------------------------------------
+
+--
 -- Struttura della tabella `clienti`
 --
 
@@ -34,15 +69,16 @@ CREATE TABLE `clienti` (
   `username` varchar(32) NOT NULL,
   `mail` varchar(64) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `indirizzo_id` int(11) NOT NULL
+  `indrizzo_id` int(11) NOT NULL,
+  `carta_credito_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dump dei dati per la tabella `clienti`
 --
 
-INSERT INTO `clienti` (`cliente_id`, `nome`, `cognome`, `username`, `mail`, `password`, `indirizzo_id`) VALUES
-(1, 'pippo', 'pluto', 'pippo_pluto', 'a@gmail.com', '4124bc0a9335c27f086f24ba207a4912', 1);
+INSERT INTO `clienti` (`cliente_id`, `nome`, `cognome`, `username`, `mail`, `password`, `indrizzo_id`, `carta_credito_id`) VALUES
+(16, 'mario', 'rossi', 'mario_rossi', 'rossi@gmail.com', '4124bc0a9335c27f086f24ba207a4912', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -51,7 +87,7 @@ INSERT INTO `clienti` (`cliente_id`, `nome`, `cognome`, `username`, `mail`, `pas
 --
 
 CREATE TABLE `indirizzi` (
-  `indirizzo_id` int(11) NOT NULL,
+  `indrizzo_id` int(11) NOT NULL,
   `via` varchar(64) NOT NULL,
   `citta` varchar(32) NOT NULL,
   `provincia` char(2) NOT NULL,
@@ -63,12 +99,37 @@ CREATE TABLE `indirizzi` (
 -- Dump dei dati per la tabella `indirizzi`
 --
 
-INSERT INTO `indirizzi` (`indirizzo_id`, `via`, `citta`, `provincia`, `regione`, `cap`) VALUES
-(1, 'via Garibaldi 11', 'Mariano Comense', 'CO', 'Lombardia', '22070');
+INSERT INTO `indirizzi` (`indrizzo_id`, `via`, `citta`, `provincia`, `regione`, `cap`) VALUES
+(1, 'via Garibaldi 11', 'Milano', 'MI', 'Lombardia', '20121');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `stazioni`
+--
+
+CREATE TABLE `stazioni` (
+  `stazione_id` int(11) NOT NULL,
+  `indirizzo_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Indici per le tabelle scaricate
 --
+
+--
+-- Indici per le tabelle `biciclette`
+--
+ALTER TABLE `biciclette`
+  ADD PRIMARY KEY (`bicicletta_id`);
+
+--
+-- Indici per le tabelle `carte_credito`
+--
+ALTER TABLE `carte_credito`
+  ADD PRIMARY KEY (`carta_credito_id`),
+  ADD UNIQUE KEY `cvv` (`cvv`),
+  ADD UNIQUE KEY `numero` (`numero`);
 
 --
 -- Indici per le tabelle `clienti`
@@ -77,29 +138,55 @@ ALTER TABLE `clienti`
   ADD PRIMARY KEY (`cliente_id`),
   ADD UNIQUE KEY `mail` (`mail`),
   ADD UNIQUE KEY `username` (`username`),
-  ADD KEY `indirizzo_id` (`indirizzo_id`);
+  ADD KEY `carta_credito_id` (`carta_credito_id`),
+  ADD KEY `indrizzo_id` (`indrizzo_id`);
 
 --
 -- Indici per le tabelle `indirizzi`
 --
 ALTER TABLE `indirizzi`
-  ADD PRIMARY KEY (`indirizzo_id`);
+  ADD PRIMARY KEY (`indrizzo_id`);
+
+--
+-- Indici per le tabelle `stazioni`
+--
+ALTER TABLE `stazioni`
+  ADD PRIMARY KEY (`stazione_id`),
+  ADD KEY `indirizzo_id` (`indirizzo_id`);
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
 --
+-- AUTO_INCREMENT per la tabella `biciclette`
+--
+ALTER TABLE `biciclette`
+  MODIFY `bicicletta_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `carte_credito`
+--
+ALTER TABLE `carte_credito`
+  MODIFY `carta_credito_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT per la tabella `clienti`
 --
 ALTER TABLE `clienti`
-  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT per la tabella `indirizzi`
 --
 ALTER TABLE `indirizzi`
-  MODIFY `indirizzo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `indrizzo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT per la tabella `stazioni`
+--
+ALTER TABLE `stazioni`
+  MODIFY `stazione_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Limiti per le tabelle scaricate
@@ -109,7 +196,8 @@ ALTER TABLE `indirizzi`
 -- Limiti per la tabella `clienti`
 --
 ALTER TABLE `clienti`
-  ADD CONSTRAINT `clienti_ibfk_1` FOREIGN KEY (`indirizzo_id`) REFERENCES `indirizzi` (`indirizzo_id`);
+  ADD CONSTRAINT `clienti_ibfk_1` FOREIGN KEY (`carta_credito_id`) REFERENCES `carte_credito` (`carta_credito_id`),
+  ADD CONSTRAINT `clienti_ibfk_2` FOREIGN KEY (`indrizzo_id`) REFERENCES `indirizzi` (`indrizzo_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
